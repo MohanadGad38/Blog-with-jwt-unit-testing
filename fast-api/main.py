60daf3132d586ed1197119087edc8256b4531bab
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-05-25 22:46:53 +0300

    run fast api

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,1 +1,14 @@
-print("ddd")
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return 'heyy'
+
+
+@app.get('/blog/{id}')
+def blog(id: int):
+    return {"data": id}
