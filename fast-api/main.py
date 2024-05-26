ce5d74284d7fa6e72f8ab67a3997b17d44dc334d
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-05-26 21:58:59 +0300

    feat:blog class

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,6 +10,25 @@ def index():
     return 'heyy'
 
 
+@app.get('/blogg')
+def bloglimit(limit=10, pub: bool = True, sorts: Optional[str] = None):
+    if pub:
+        return {'data': f'{limit} published blogs from the db '}
+    else:
+        return {'data': f'{limit} blogs from db '}
+
+
 @app.get('/blog/{id}')
 def blog(id: int):
     return {"data": id}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    publish: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {"data": f"Blog is created !! {request.title}"}
