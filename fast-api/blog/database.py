d7c06bdc3c8bcf27e3d87a69f1c5a849982da747
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-05-28 23:02:58 +0300

    feat:creating models

--- fast-api/blog/database.py
+++ fast-api/blog/database.py
@@ -1,4 +1,10 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 SQLALCHAMY_DATABASE_URL = "sqlite://./blog.db"
-engine = create_engine(SQLALCHAMY_DATABASE_URL)
+engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={
+                       "check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
