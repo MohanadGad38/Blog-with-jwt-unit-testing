575660851a364a00b7e3d77972960dcd1e541a8c
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-06-01 01:21:58 +0300

    users

--- fast-api/blog/main.py
+++ fast-api/blog/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Depends, status, Response
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 import uvicorn
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
-# models.Base.metadata.create_all(bind=engine)
+from typing import List
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -25,21 +26,57 @@ def create(request: schemas.blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get("/blog", status_code=status.HTTP_201_CREATED)
+@app.get("/blog", status_code=status.HTTP_201_CREATED, response_model=List[schemas.show_blog])
 def fetch(db: Session = Depends(get_db)):
     get_blog = db.query(models.Blog).all()
 
     return get_blog
 
-# if i use .first i will get one object , if i used .all() i will get one object inside a list exxxcept if i have many rows with same id
+# if i use .first i will get one object , if i used .all() i will get one object inside a list except if i have many rows with same id
 
 
-@app.get(f"/blog/{id}", status_code=201)
+@app.get(f"/blog/{id}", status_code=201, response_model=schemas.show_blog)
 def fetch_one(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
-        response.status_code = status.HTTP_404_NOT_FOUND
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="not found")
     return blog
+
+
+# delete(synchronize_session=False)
+@app.delete(f"/blog/{id}")
+def delete_blog(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="blog not avabile ")
+    blog.delete(
+        synchronize_session=False)
+    db.commit()
+    return "deleted"
+
+
+@app.put(f"/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id, request: schemas.blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="blog not avabile ")
+    blog.update(request.dict())
+    db.commit()
+    return "updated"
+
+
+@app.post("/user")
+def create_user(request: schemas.user, db: Session = Depends(get_db)):
+    new_user = models.Users(
+        name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    db.close()
+    return request
 
 
 if __name__ == "__main__":
--- fast-api/blog/schemas.py
+++ fast-api/blog/schemas.py
@@ -4,3 +4,14 @@ from pydantic import BaseModel
 class blog(BaseModel):
     title: str
     body: str
+
+
+class show_blog(blog):
+    class Config():
+        orm_mode = True
+
+
+class user(BaseModel):
+    name: str
+    email: str
+    password: str
